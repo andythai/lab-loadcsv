b26e5b13609b27b9ac2614cac78f0b410ec86935
Andy Thai <andy.thai9@gmail.com>
2017-01-10 18:37:22 -0800

    Add files via upload

--- pulldata.m
+++ pulldata.m
@@ -1,6 +1,9 @@
 % Function pulls data from csv files
-% Input: A table imported from ChronoViz
-function pulldata(csvfile)
+% Input: 
+%   A table imported from ChronoViz
+% Output: 
+%   T - table to return
+function T = pulldata(csvfile)
 
 % Initialize variables
 START_TIME_OFFSET = 1; % Location of start time column
@@ -8,12 +11,19 @@ END_TIME_OFFSET = 2;   % Location of end time column
 LABEL_OFFSET = 5;      % Location of labeled title
 TIME_FORMAT = 'MM:SS.FFF';
 NUM_ROWS = size(csvfile,1);
+amount = NUM_ROWS - 1;
+
+% Variables for table output
+T = cell2table(cell(amount, 3));
+T.Properties.VariableNames{1} = 'Entry';
+T.Properties.VariableNames{2} = 'Seconds';
+T.Properties.VariableNames{3} = 'Label';
 
 % Initialize vectors
 start_time_vector = csvfile(:, START_TIME_OFFSET);
 end_time_vector = csvfile(:, END_TIME_OFFSET);
 label_vector = csvfile(:, LABEL_OFFSET);
-elapsed_time_vector = zeros(NUM_ROWS - 1, 1);
+elapsed_time_vector = zeros(amount, 1);
 
 % Calculates elapsed time
 for index = 2:NUM_ROWS
@@ -24,9 +34,13 @@ end
 
 % Print output
 fprintf('Analyzing %s data\n', inputname(1));
-fprintf('Number of entries: %d\n', NUM_ROWS - 1);
+fprintf('Number of entries: %d\n', amount);
 fprintf('Elapsed time values:\n');
-for index = 1:NUM_ROWS - 1
+for index = 1:amount
     fprintf('%d \t %.1f sec \t ', index, elapsed_time_vector(index));
+    T{index,1} = {index};
+    T{index,2} = {elapsed_time_vector(index)};
+    T{index,3} = {char(label_vector{index + 1, 1})};
     fprintf('%s\n', char(label_vector{index + 1, 1}));
 end
+
